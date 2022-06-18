92c0801905e4fba6ed85da899b83e15cd696e568
MarceloDutra <marcellodutra010@yahoo.com.br>
2022-06-18 20:14:48 -0300

    jump tabela imc

---
+++ Criando uma pagina html_css/Exercicio_IMC/Resumo.js
@@ -0,0 +1,30 @@
+/* Para criarmos as funcionalidades de calculo do imc fazemos o seguinte:
+
+-Criamos variaveis para cada input criado no html para linkarmos com o js.
+
+-Criamos uma funcao para qual queremos que é imc pode-se aplicar este valor a funcao.
+
+-Dentro desta funcao colocamos apenas os inputs em escopo exclusivo da funcao e global so o do resultado(button).
+
+- Criamos um condicional onde os testes tem que ser peso e altura !==(diferente de cx de text vazia) ex:
+if(peso.value !== '' && altura !== '' ).OBS: o value pode ser aplicado direto na variavel no final dela não precisando colocar ali dentro da condicional.
+
+-Criamos uma variavel para valorImc onde este valor e calculado da seguinte forma:
+const valorImc = (peso / altura * altura). Onde altura ao quadrado e multiplicado por ela mesma.
+
+-Depois desta variavel criaremos um let para pode ser sobrescrito onde vamos condicionar as classificações ex:
+let classificação = '' string vazia porque vai ser sobrescrita nas condicionais com outras frases.
+
+- Criamos entao as condicionais com if e else onde vamos chamar o valor da variavel valorImc e colocar os testes ex:
+if(valorImc > 18.5){
+    classificação = 'abaixo do peso'... e assim vamos realizando. 
+}
+
+- Ao final desta condicional vamos colocar o seguinde imprimir o resultado do valor do imc ex:
+resultado.innerTexContent = valorImc.
+
+- depois um else para imprimir o resultado caso nao seja preenchido nada nas caixas e mostrar pro usuario que deve preencher ex:
+resultado.innerTextContent = 'preencha os campos'
+
+- Ao final a ação do button para que este calculo aconteça utilizando a seguinte sintaxe ex:
+fora do escopo da funcao calculo.addVentListene('click', imc). Onde ele vai dar acao ao clicar e dizer o que vai calcular que e o imc(funcao criada la no topo com as variaveis.)*/
---
+++ Criando uma pagina html_css/Exercicio_IMC/script.js
@@ -0,0 +1,35 @@
+const calcular = document.getElementById('calcular')//Fic de forma global.
+
+//Cria-se uma funcao imc.
+function imc(){
+    const peso = document.getElementById('num01')
+    const altura = document.getElementById('num02')
+    const res = document.getElementById('res')
+
+    //condicional de teste.
+    if(peso.value !== '' && altura.value !== ''){
+        
+        const valorImc = (peso / altura * altura)
+
+        let classPeso = ''
+
+        if(valorImc < 18.5){
+            classPeso = 'Esta abaixo do peso'
+        }else if(valorImc >= 18.5 && valorImc <= 24.9){
+            classPeso = 'Peso normal'
+        }else if(valorImc >= 25 && valorImc <= 29.9){
+            classPeso = 'Sobre peso'
+        }else if(valorImc >= 30 && valorImc <= 34.0){
+            classPeso = 'Obesidade grau 1'
+        }else if(valorImc >= 35 && valorImc <= 39.0){
+            classPeso = 'Obesidade grau 2'
+        }else{
+            classPeso = "Obesidade grau 3"
+
+            res.innerTextContent = valorImc
+        }
+    }else{
+        res.innerTextContent = 'Invalido. Preencha os campos!'
+    }
+}
+calcular.addEventListener('click', imc)
